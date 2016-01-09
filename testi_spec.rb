6f20e4cdf8c93020d7cfc04fea33b116367ecf51
Matti Luukkainen <mluukkai@iki.fi>
2016-01-09 14:10:11 +0200

    fix test

--- testi_spec.rb
+++ testi_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'metodi kertoma' do
     method_exists(self, :tulo, 1, 1)
     expect(tulo(1, 1)).to eq(1)
     expect(tulo(3, 2)).to eq(6)
-    expect(tulo(4, 5)).to eq(24)
+    expect(tulo(4, 5)).to eq(20)
     expect(tulo(6, 10)).to eq(60)
     expect(tulo(4, -2)).to eq(-8)
   end
