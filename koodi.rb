3e49d60dba07a489e8c06ccf622bcb568b0bd1ac
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-01-21 19:26:21 +0200

    Arrayn tulostus futaa

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,9 @@
-# kirjoita koodi tänne
+def tulosta(taulukko)
+  printti = ""
+  0.upto((taulukko.length)-1) do |i|
+    printti = printti + taulukko.at(i).to_s
+  end
+  puts printti
+end
+
+tulosta([1, "asd", true])
